4a0fd4441059d3ddbf634ec74033fc18cf9bf2cb
Marcos Peter <mpeterlobato@gmail.com>
2022-11-17 19:12:38 -0300

    Finishing the users list frontend

---
+++ src/pages/home/mocked-data.ts
@@ -0,0 +1,127 @@
+export const mockedData = [
+  {
+    id: 1,
+    name: "Sawyer Lester",
+    email: "libero.mauris@protonmail.com",
+  },
+  {
+    id: 2,
+    name: "Karly Baird",
+    email: "arcu.vivamus@google.com",
+  },
+  {
+    id: 3,
+    name: "Nash Richardson",
+    email: "non.hendrerit@google.couk",
+  },
+  {
+    id: 4,
+    name: "Vladimir Conley",
+    email: "lacus.vestibulum@yahoo.org",
+  },
+  {
+    id: 5,
+    name: "Isaiah Hale",
+    email: "diam@outlook.com",
+  },
+  {
+    id: 6,
+    name: "Damian Hardin",
+    email: "iaculis@outlook.edu",
+  },
+  {
+    id: 7,
+    name: "Fritz Fox",
+    email: "felis@yahoo.edu",
+  },
+  {
+    id: 8,
+    name: "Francesca Aguirre",
+    email: "sed.sem@protonmail.ca",
+  },
+  {
+    id: 9,
+    name: "Kyle Randolph",
+    email: "lobortis.nisi.nibh@outlook.ca",
+  },
+  {
+    id: 10,
+    name: "Kenneth England",
+    email: "a@icloud.ca",
+  },
+  {
+    id: 11,
+    name: "Tarik Robles",
+    email: "nulla@yahoo.net",
+  },
+  {
+    id: 12,
+    name: "Rosalyn Cash",
+    email: "consequat.nec@aol.ca",
+  },
+  {
+    id: 13,
+    name: "Zephania Acosta",
+    email: "sagittis.lobortis.mauris@yahoo.edu",
+  },
+  {
+    id: 14,
+    name: "Cassidy Estes",
+    email: "malesuada.integer@hotmail.com",
+  },
+  {
+    id: 15,
+    name: "Olympia Hoover",
+    email: "scelerisque.lorem.ipsum@aol.edu",
+  },
+  {
+    id: 16,
+    name: "Melanie Woods",
+    email: "consequat.purus@yahoo.org",
+  },
+  {
+    id: 17,
+    name: "Elliott Mcgowan",
+    email: "enim.suspendisse@protonmail.couk",
+  },
+  {
+    id: 18,
+    name: "Zorita Carney",
+    email: "adipiscing.elit.aliquam@protonmail.org",
+  },
+  {
+    id: 19,
+    name: "Travis Chambers",
+    email: "dictum.ultricies.ligula@protonmail.couk",
+  },
+  {
+    id: 20,
+    name: "Melinda Marquez",
+    email: "non.vestibulum@protonmail.org",
+  },
+  {
+    id: 21,
+    name: "Stewart Townsend",
+    email: "risus.duis.a@icloud.edu",
+  },
+  {
+    id: 22,
+    name: "Fredericka Blackburn",
+    email: "etiam.laoreet@protonmail.edu",
+  },
+  {
+    id: 23,
+    name: "Jelani Park",
+    email: "libero.donec@protonmail.net",
+  },
+  {
+    id: 24,
+    name: "Henry Holden",
+    email: "diam@icloud.org",
+  },
+  {
+    id: 25,
+    name: "Gregory Nichols",
+    email: "libero.est.congue@google.couk",
+  },
+];
